dc4376154eecb0a62d3a422a695b22f3e0c8bc1f
Charles Chen <chen.charles.c@gmail.com>
2025-06-18 22:43:59 -0400

    Added endpoint to execute code

--- server/Program.cs
+++ server/Program.cs
@@ -1,8 +1,13 @@
 ﻿using System.ComponentModel;
+using Jint;
 using Microsoft.AspNetCore.Builder;
 using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Logging;
 using ModelContextProtocol.Server;
+using OpenTelemetry;
+using OpenTelemetry.Metrics;
+using OpenTelemetry.Resources;
+using OpenTelemetry.Trace;
 
 var builder = WebApplication.CreateBuilder(args);
 
@@ -14,6 +19,20 @@ builder.Logging.AddConsole(consoleLogOptions =>
 
 builder.Services.AddMcpServer().WithHttpTransport().WithToolsFromAssembly();
 
+// OpenTelemetry configuration for visibility (http://localhost:18888)
+builder
+    .Services.AddOpenTelemetry()
+    .ConfigureResource(r => r.AddService("runjs:server"))
+    .WithTracing(b =>
+        b.AddSource("*")
+            .AddAspNetCoreInstrumentation()
+            .AddHttpClientInstrumentation()
+            .ConfigureResource(r => r.AddService("runjs:server:http"))
+    )
+    .WithMetrics(b => b.AddMeter("*").AddAspNetCoreInstrumentation().AddHttpClientInstrumentation())
+    .WithLogging()
+    .UseOtlpExporter();
+
 var app = builder.Build();
 
 app.MapMcp();
@@ -23,10 +42,40 @@ app.Run();
 [McpServerToolType]
 public static class EchoTool
 {
-    [McpServerTool(Name = "echo"), Description("Echoes the message back to the client.")]
+    [
+        McpServerTool(Name = "echo"),
+        Description(
+            "Echoes the message back to the client; only use this if the user explicitly asks for an echo."
+        )
+    ]
     public static string Echo(string message)
     {
         Console.WriteLine($"Echoing message: {message}");
         return $"Hello to you! {message}";
+    }
+}
+
+[McpServerToolType]
+public static class JintTool
+{
+    [
+        McpServerTool(Name = "runJavaScript"),
+        Description("Runs JavaScript code and returns the result.")
+    ]
+    public static string RunJavaScriptCode(
+        [Description("The JavaScript code to execute; returns 'void' if there is no result")]
+            string code
+    )
+    {
+        var engine = new Engine(options =>
+        {
+            options.LimitMemory(1_000_000); // 1 MB
+            options.TimeoutInterval(TimeSpan.FromSeconds(5));
+            options.MaxStatements(500);
+        });
+
+        var result = engine.Execute(code);
+
+        return result?.ToString() ?? "void";
     }
 }
