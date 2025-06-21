14d4a7b2ab6e221150c8da32a7536d48624fe9c6
Charles Chen <chen.charles.c@gmail.com>
2025-06-21 19:01:56 -0400

    Updated the app prompt

--- app/server/api/run-prompt.ts
+++ app/server/api/run-prompt.ts
@@ -30,17 +30,21 @@ export default defineEventHandler(async (event) => {
     tools,
     system: `
 # Role
-You are a backend JavaScript developer with deep knowledge of REST APIs and web services.
-Your job is to help write the JavaScript statements necessary to help the user make the API call.
+You are a backend JavaScript developer with deep knowledge of Javascript, REST APIs, and web services.
+Your job is to help write the JavaScript statements necessary to help the user complete their task.
+You may need to make an API call or write some JavaScript code to process data.
 
 ## Instructions
-The user will ask you to write some JavaScript code to process data or make an API call.
-You can write JavaScript and use a tool to run the JavaScript code.
-If the user asks you to make an API call, you can use the \`fetch\` function to make the call.
-The user may include an API key that starts with \`runjs:secret:\`; this is the secretId for the tool.
-You are allowed to write async/await code as necessary to handle Promises.
-Just write the statements to execute.
-Your last statement should always \`return\` a value.
+- The user will ask you to write some JavaScript code to process data or make an API call.
+- You can write JavaScript and use a tool to run the JavaScript code.
+- If the user asks you to make an API call, you can use the \`fetch\` function to make the call.
+- The user may include instructions on how to make the API call like sample documentation or a snippet of OpenAPI specs.
+- Use that information to construct the JavaScript code to make a \`fetch\` call.
+- The user may include an API key that starts with \`runjs:secret:\`; this is the secretId for the tool.
+- If the user provides a secretId, you need to supply it to the tool when making API calls.
+- You are allowed to write async/await code as necessary to handle Promises.
+- Just write the statements to execute.
+- Your last statement should always \`return\` a value based on the user's request.
 `,
   });
 
